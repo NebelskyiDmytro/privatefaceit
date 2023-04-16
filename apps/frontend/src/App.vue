5dfe278d92998ee12c6f3a233fac3680cf30388e
Dmytro Nebelskyi <dmytro.nebelskyi@gmail.com>
2023-04-16 15:00:31 +0300

    Opacity changed and gitignore modified

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -157,7 +157,7 @@ sse.onmessage = (event) => {
 <style>
 .app {
   transition: background-color 0.6s ease 0s;
-  background: rgba(0, 0, 0, 0.90);
+  background: rgba(0, 0, 0, 0.10);
 }
 
 .widget {
